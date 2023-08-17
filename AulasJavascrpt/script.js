1dcae8e9933ad3df2db5578fccd73ac568bb12c7
guualvees <gugomes1995@yahoo.com.br>
2023-08-17 11:13:34 -0300

    17/08

--- AulasJavascrpt/script.js
+++ AulasJavascrpt/script.js
@@ -1,9 +1,8 @@
-let rlSync = require ('readline-sync');
-let nome = rlSync.question ("Qual o seu nome?\n")
-console.log (`Olá, ${nome}`)
-//bananas1
-//bananas2
-//bananas3
+let rlSync = require('readline-sync');
+let nome = rlSync.question("Qual o seu nome?\n")
+console.log(`Olá, ${nome}`)
+
+
 
 
 
