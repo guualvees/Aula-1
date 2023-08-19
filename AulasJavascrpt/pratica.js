f713a37cdfc86dd970effea10c4165d8f434ea59
guualvees <gugomes1995@yahoo.com.br>
2023-08-19 01:39:08 -0300

    19/08

---
+++ AulasJavascrpt/pratica.js
@@ -0,0 +1,13 @@
+console.log("praticando")
+var a = 10;
+var b = 20;
+var total = a + b;
+console.log(total);
+
+var nomePessoa = "Gustavo";
+console.log(nomePessoa)
+
+var var1 = 1,
+    var2 = 2,
+    var3 = 3;
+console.log(var2)  
